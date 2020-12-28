51fabfee1601f4225db79be1a50c8690bea8df79
KayKay-git <kayalew@yahoo.com>
2020-12-27 23:36:33 -0500

    Updated Board component; completed generateSquareComponents function

--- src/App.js
+++ src/App.js
@@ -27,15 +27,13 @@ const generateSquares = () => {
 
 const App = () => {
 
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
+  // This starts state off as a 2D array of JS objects with empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
 
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
-
 
   const checkForWinner = () => {
     // Complete in Wave 3
@@ -62,6 +60,7 @@ const App = () => {
         <button>Reset Game</button>
       </header>
       <main>
+{/* App should pass to Board a 2D array of JavaScript objects  */}
         <Board squares={squares} />
       </main>
     </div>
@@ -69,3 +68,4 @@ const App = () => {
 }
 
 export default App;
+
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,15 +6,24 @@ import PropTypes from 'prop-types';
 
 const generateSquareComponents = (squares, onClickCallback) => {
   // Complete this for Wave 1
-  // squares is a 2D Array, but 
-  //  you need to return a 1D array
-  //  of square components
-
+  // App will pass to Board a 2D array of JavaScript objects and Board will use that to render an array of Square components.
+  // squares is a 2D Array, and we need to return a 1D array of square components
+  // Each Square component should take 3 props. id, value and onclickcallback
+  
+  const flattenedSq = squares.flat()
+  return flattenedSq.map((props) =>
+    <Square 
+      id = {props.id}
+      value = {props.value}
+      onClickCallback = {onClickCallback}
+      key = {props.id}
+    />
+  );
 }
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
+  // console.log(squareList);
   return <div className="grid" >
     {squareList}
   </div>
