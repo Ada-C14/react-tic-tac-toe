b720c79f9539e0e28c04ccf06ec96804eab05c64
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-29 16:45:21 -0500

    wave1 grid working

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,6 +9,29 @@ const generateSquareComponents = (squares, onClickCallback) => {
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
+  const squaresArray = []
+  // squares.length.times do |row| 
+  //   row.length.times do |col|
+  
+
+  // const Timeline = (props) => {
+
+  //   const timelineEvents = props.events.map((event, i) => {
+  //     return (
+  //         <TimelineEvent key={i} person={event.person} status={event.status} timeStamp={event.timeStamp}/>
+  //     );
+  //   });
+  // 
+  console.log(squares)
+
+    for (const row of squares) {
+      for (const column of row) {
+        squaresArray.push(<Square key={column.id} id={column.id} value={column.value}/>);
+      }
+    }
+    return squaresArray;
+// squares = squaresArray
+
 
 }
 
