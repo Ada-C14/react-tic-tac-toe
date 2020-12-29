79fbc4ef5c5b967cfd117808555d18a79bd5f2dc
Li Dai <li.lea.dai@gmail.com>
2020-12-29 11:56:42 -0800

    rendered the 2D array to return a 1D array of squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,6 +9,33 @@ const generateSquareComponents = (squares, onClickCallback) => {
   // squares is a 2D Array, but 
   //  you need to return a 1D array
   //  of square components
+  const totalRows = [];
+  for (let row = 0; row < 3; row += 1) {
+    const singleRow = [];
+    for (let col = 0; col < 3; col += 1) {
+      const currentSquare = squares[row][col];
+      singleRow.push(
+        <div key={col}>
+          <Square key={currentSquare.id}
+          id={currentSquare.id}
+          value={currentSquare.value}
+          onClickCallback={onClickCallback} />
+        </div>
+
+      );
+    }
+    totalRows.push(
+      <div key={row}>
+      {singleRow}
+      </div>
+    );
+  }
+  return (
+  <div>
+  {totalRows}
+  </div>
+  )
+
 
 }
 
