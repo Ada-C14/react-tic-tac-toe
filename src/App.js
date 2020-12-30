93d7ab95093510b8d04b48a2645761ec57c27712
Sophia Donan <sophiadonan@gmail.com>
2020-12-30 15:15:02 -0800

    started working on checkForWinner

--- src/App.js
+++ src/App.js
@@ -45,8 +45,19 @@ function switchPlayer() {
   changeCurrent(!current);
   return current;
 };
-  const checkForWinner = () => {
-  
+  const checkForWinner = (squares) => {
+    const possibleWinners = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const 
   
 
   }
