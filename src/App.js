84c77c6c8f35540b67bd7668a67476cccb0d1365
dnsrocha <drocha@gmail.com>
2020-12-30 15:06:26 -0800

    starting wave 3

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Board from './components/Board';
 
-const PLAYER_1 = 'X';
-const PLAYER_2 = 'O';
+const PLAYER_1 = 'x';
+const PLAYER_2 = 'o';
 
 const generateSquares = () => {
   const squares = [];
@@ -26,22 +26,17 @@ const generateSquares = () => {
 }
 
 const App = () => {
-
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
   const [player, setPlayer] = useState(PLAYER_1);
+  const [winner, setWinner] = useState(null);
 
   // Wave 2
-  // You will need to create a method to change the square 
-  //   When it is clicked on.
-  //   Then pass it into the squares as a callback
   const updateSquare = (updatedSquare) => {
     const newSquares = [...squares]
 
     for(let row of newSquares) {
       for(let square of row) {
-        if (square.id === updatedSquare.id && square.value === '') {
+        if (square.id === updatedSquare.id && square.value === '' && winner === null) {
             square.value = player;          
         }
       }
@@ -50,18 +45,34 @@ const App = () => {
     setPlayer(player === PLAYER_1 ? PLAYER_2 : PLAYER_1)
 
     setSquares(newSquares);
+
+    setWinner(checkForWinner());
+    
   }
 
   const checkForWinner = () => {
     // Complete in Wave 3
-    // You will need to:
-    // 1. Go accross each row to see if 
-    //    3 squares in the same row match
-    //    i.e. same value
-    // 2. Go down each column to see if
-    //    3 squares in each column match
-    // 3. Go across each diagonal to see if 
-    //    all three squares have the same value.
+
+    const winPossibilities = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    let allSquares = squares.flat()
+
+    winPossibilities.forEach((possibility) => {
+      let rowOfThree = []
+      possibility.forEach((i) => {
+        rowOfThree.push(allSquares[i])
+      })
+      if (rowOfThree.every(obj => obj.value === 'x')) {
+        console.log('Winner is x');
+        return PLAYER_1
+        
+      } else if (rowOfThree.every(obj => obj.value === 'o')) {
+        console.log('Winner is o');
+        return PLAYER_2
+      } else if (allSquares.every(obj => obj.value !== '')) {
+        console.log('Tie');
+        return null
+      }
+    })
 
   }
 
@@ -73,7 +84,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>The winner is {winner} </h2>
         <button>Reset Game</button>
       </header>
       <main>
