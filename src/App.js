2307f9d6bee72c06eb13b75771bf5a3fc03d9426
Taylor Tompkins <taylormililani@gmail.com>
2020-12-30 14:28:35 -0800

    one weird test not passing

--- src/App.js
+++ src/App.js
@@ -31,13 +31,36 @@ const App = () => {
   // empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1)
-
+  const [winner, setWinner] = useState(null)
+  const [filledSquares, setFilledSquares] = useState(0);
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
 
+
+  const checkForWinner = () => {
+    let i = 0;
+    while (i < 3) {
+      if (squares[i][0].value === squares[i][1].value &&  squares[i][1].value === squares[i][2].value && squares[i][0] !== '') {
+        return squares[i][0].value;
+      } else if (squares[0][i].value === squares[1][i].value &&  squares[1][i].value === squares[2][i].value && squares[0][i] !== '') {
+        return squares[0][i].value;
+      }
+      i += 1;
+    } 
+    
+
+    if (squares[0][0].value === squares[1][1].value &&  squares[1][1].value === squares[2][2].value && squares[0][0] !== '') {
+      return squares[0][0].value;
+    } else if (squares[0][2].value === squares[1][1].value &&  squares[1][1].value === squares[2][0].value && squares[0][0] !== '') {
+      return squares[0][2].value;
+    }
+    return null;
+  }
+  
   // const updateSquares = (id) => {
+  //   if (winner !== null) return;
   //   const updated = [...squares]
   //   let r = 0;
   //   let c = 0;
@@ -59,46 +82,36 @@ const App = () => {
   //     r += 1
   //     c = 0
   //   } while (r < 3)
+  //   setFilledSquares(filledSquares + 1);
   //   setSquares(updated)
+  //   setWinner(checkForWinner());
   // } 
- 
   const changeSquareUsingSetSquares = (id) => {
+    // if (winner !== null) return;
     let foundSquare = squares.flat().find(square => square.id === id);
-    if (foundSquare.value === '') {
-      foundSquare.value = currentPlayer;
-    } else {
-      return;
-    }
-    setSquares(squares);
+    if (foundSquare.value !== '') return;
+  
+    foundSquare.value = currentPlayer;
 
     let nextPlayer = currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1;
     setCurrentPlayer(nextPlayer);
-  }
-
-    const checkForWinner = () => {
-    // Complete in Wave 3
-    // You will need to:
-    // 1. Go accross each row to see if 
-    //    3 squares in the same row match
-    //    i.e. same value
-    // 2. Go down each column to see if
-    //    3 squares in each column match
-    // 3. Go across each diagonal to see if 
-    //    all three squares have the same value.
-
+    setSquares(squares);
+    setFilledSquares(filledSquares + 1);
+    setWinner(checkForWinner());
   }
 
   const resetGame = () => {
     setSquares(generateSquares);
     setCurrentPlayer(PLAYER_1);
+    setWinner(null);
+    setFilledSquares(0);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        {/* <h2>The winner is ... -- Fill in for wave 3 </h2> */}
-        <h2>{currentPlayer}</h2>
+        <h2>{winner === null ? `Player ${ currentPlayer }` : `Winner is ${ winner }`}</h2>
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
