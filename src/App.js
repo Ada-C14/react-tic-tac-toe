55dd6ba3da2763302110dbd03581b53abb65910b
Ayesha Asif <ayaseef@gmail.com>
2020-12-30 15:47:29 -0800

    Added wave 4 functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Board from './components/Board';
 
 const PLAYER_1 = 'X';
+
 const PLAYER_2 = 'O';
 
 const generateSquares = () => {
@@ -13,7 +14,7 @@ const generateSquares = () => {
 
   for (let row = 0; row < 3; row += 1) {
     squares.push([]);
-    for (let col = 0; col < 3; col += 1) {
+    for (let column = 0; column < 3; column += 1) {
       squares[row].push({
         id: currentId,
         value: '',
@@ -39,45 +40,62 @@ const App = () => {
     newSquares.push(squares[row]);
   }
 setSquares(newSquares);
-} // => change/ remove bracket
+// => change/ remove bracket
 //toggle players
-function switchPlayer() {
-  changeCurrent(!current);
-  return current;
-};
-  const checkForWinner = (squares) => {
-    const possibleWinners = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    const 
-  
-
+  function switchPlayer() {
+    changeCurrent(!current);
+    return current;
+  };
+const checkForWinner = () => {
+  if(squares[0][0].value === squares[1][1].value  && squares[0][0].value === squares[2][2].value && squares[0][0].value !== '' ){
+  return squares[0][0].value;
+  }else if(squares[0][2].value === squares[1][1].value && squares[0][2].value === squares[2][0].value && squares[0][2].value !== ''){
+  return squares[0][2].value;
+  }else{
+    for(let row = 0; row < squares.length; row += 1){
+      for(let column = 0; column < squares.length; column += 1){
+        if(squares[row][0].value === squares[row][1].value && squares[row][0].value === squares[row][2].value && squares[row][0].value !== '' ){
+          return squares[row][0].value
+        }else if(squares[0][column].value === squares[1][column].value && squares[0][column].value === squares[2][column].value && squares[0][column].value !== ''){
+          return squares[0][column].value;
+        }
+      }
+    }
   }
+  return -1;
+};
 
-  const resetGame = () => {
-    // Complete in Wave 4
+
+const resetGame = () => {
+setSquares(generateSquares());
+}
+let winner = checkForWinner();
+  if(winner !== -1){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>React Tic Tac Toe</h1>
+          <h2>The winner is {winner} </h2>
+          <button>Reset Game</button>
+        </header>
+        <main>
+          <p>WoHOO!!!</p>
+        </main>
+      </div>
+    );
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
-        <button>Reset Game</button>
+        <h2>Game is still on... </h2>
+        <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback={onClickCallback}/>
       </main>
     </div>
   );
-}
-
+} 
 export default App;
