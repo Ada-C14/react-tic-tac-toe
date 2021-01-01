664dac0ae00d0f0bad9b6477db059781da717e67
Leah Scott-Zechlin <scottzec@gmail.com>
2020-12-31 17:29:56 -0800

    prunes comments

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Board from './components/Board';
 const PLAYER_1 = 'x';
 const PLAYER_2 = 'o';
 
-// You need to debug React in the browser because changes are happening in the browser
-
 const generateSquares = () => {
   const squares = [];
 
@@ -29,13 +27,11 @@ const generateSquares = () => {
 
 const App = () => {
 
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
-  const [squares, setSquares] = useState(generateSquares()); // squares is updated here, has its new value
+  const [squares, setSquares] = useState(generateSquares()); // squares is created here
   const [player, setPlayer] = useState(PLAYER_1);
   const [winner, setWinner] = useState(`...`);
 
-  // until the new render has occurred, the squares reported in onClickCallback is the same squares set up by the previous render
+  // until the new render has occurred, the squares reported in onClickCallback are the same squares set up by the previous render
   const onClickCallback = (id) => {
     const updatedSquares = generateSquares();
 
@@ -45,7 +41,6 @@ const App = () => {
       for (let col = 0; col < 3; col += 1) {
         if (updatedSquares[row][col].id === id && squares[row][col].value === '') {
         updatedSquares[row][col].value = player;
-        // console.log(updatedSquares)
         setPlayer(player === PLAYER_1 ? PLAYER_2 : PLAYER_1);
         }
         else {
@@ -55,22 +50,12 @@ const App = () => {
     }
 
     setSquares(updatedSquares);
-    // console.log(squares)
     checkForWinner(updatedSquares);
   }
 
-  // Could also checkForWinner right before rendering
-
-  // Wave 2
-  // You will need to create a method to change the square 
-  //   When it is clicked on.
-  //   Then pass it into the squares as a callback
-
   const checkForWinner = (squares) => {
 
-    // console.log(squares)
-    const flattenedArray = [].concat(...squares).flat();
-    // console.log(flattenedArray)
+    const flattenedArray = [].concat(...squares);
 
     if (flattenedArray[0].value === flattenedArray[1].value && flattenedArray[1].value === flattenedArray[2].value && flattenedArray[0].value !== '') {
       setWinner(flattenedArray[0].value);
@@ -84,11 +69,9 @@ const App = () => {
     else if (flattenedArray[0].value === flattenedArray[3].value && flattenedArray[3].value === flattenedArray[6].value && flattenedArray[0].value !== '') {
       setWinner(flattenedArray[0].value);
     }
-    // Center Column
     else if (flattenedArray[1].value === flattenedArray[4].value && flattenedArray[4].value === flattenedArray[7].value && flattenedArray[1].value !== '') {
       setWinner(flattenedArray[1].value);
     }
-    // Right Column
     else if (flattenedArray[2].value === flattenedArray[5].value && flattenedArray[5].value === flattenedArray[8].value && flattenedArray[2].value !== '') {
       setWinner(flattenedArray[2].value);
     }
@@ -98,65 +81,25 @@ const App = () => {
     else if (flattenedArray[2].value === flattenedArray[4].value && flattenedArray[4].value === flattenedArray[6].value && flattenedArray[2].value !== '') {
       setWinner(flattenedArray[2].value);
     }
-    // Complete in Wave 3
-    // You will need to:
-    // 1. Go accross each row to see if 
-    //    3 squares in the same row match
-    //    i.e. same value
-    // 2. Go down each column to see if
-    //    3 squares in each column match
-    // 3. Go across each diagonal to see if 
-    //    all three squares have the same value.
-
-    // Also: cease responding to clicks when winnner declared.
-    // Q: How do you tie in tictactoe?? I only know about draws and wins??
-
-    // winning combinations: 
-    // - three across (row) => [row][0], [row][1], [row][2]
-    // - three up and down (col) => [col][0], [col][1], [col][2]
-    // - two diagonal options => [0][0], [1][1], [2][2] && [0][2], [1][1], [2][0]
-    
-    // for(let i = 0; i < squares.length; i ++){
-    //     // check for a winning row
-    //     if (squares[i][0].value !== '' && squares[i][0].value === squares[i][1].value && squares[i][1].value === squares[i][2].value){
-    //       setWinner(squares[i][0].value);
-    //       break;
-    //     } 
-    //     // check for a winning column
-    //     else if (squares[0][i].value !== '' && squares[0][i].value === squares[1][i].value && squares[1][i].value === squares[2][i].value){
-    //       setWinner(squares[0][i].value); 
-    //       break;
-    //     } 
-    //     // check for diagonals  
-    //     else if (squares[0][0].value !== '' && squares[0][0].value === squares[1][1].value && squares[1][1].value === squares[2][2].value){
-    //       setWinner(squares[0][0].value); 
-    //       break;
-    //     } 
-    //     else if (squares[0][2].value !== '' && squares[0][2].value === squares[1][1].value && squares[1][1].value === squares[2][0].value){
-    //       setWinner(squares[0][2].value);
-    //       break;
-    //     }     
-    // }
   }
 
   const resetGame = () => {
-    // Complete in Wave 4
     setPlayer(PLAYER_1);
     setSquares(generateSquares());
     setWinner(`...`);
   }
-  // squares is updated here. You could also call checkForWinner here with squares directly
+
+  // squares is updated here. You could also call checkForWinner here right before rendering, passing squares instead of updatedSquares
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        {/* <h2>Current Player: {player} </h2> */}
         <h2>Winner is {winner} </h2>
         <button onClick={() => {resetGame()}}>Reset Game</button>
       </header>
       <main>
         <Board squares={squares} onClickCallback={onClickCallback}/> 
-        {/* prop name that is being passed down to board={the function that is defined above bc this is JS within JSX} */}
+        {/* Prop name that is being passed down to board={the function that is defined above bc this is JS within JSX} */}
         {/* Beginning of the chain reaction where we hand the updateSquare method in as a prop to Board */}
       </main>
     </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,31 +3,12 @@ import PropTypes from 'prop-types';
 
 import './Square.css'
 
-
-  // For Wave 2 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
-
-// const onSquareClick = (props) => {
-  // const updatedSquare = {
-  //   value: props.value,
-  //   onClickCallback: props.onClickCallback,
-  //   id: props.id
-  // }
-
-  // // It will go back to App & hand it an updated square. (calls the function passed from App.js
-  // props.onClickCallback(updatedSquare);
-  // // onClickCallback(updatedSquare);
-// }
-  // When you click, that event listener will go back to App
+  // When you click, the event listener will go back to App.js
 const Square = (props) => {
   return <button 
     className="square"
-    onClick={() => {props.onClickCallback(props.id)}} 
-  // Callback function is called here on Squre then cascades up
-  // Anonymous function prevents it from being executed immediately
-  // anon function does the same as onsquareclick
-    
+    onClick={() => {props.onClickCallback(props.id)}} // Event handler
+  // Callback function is called here on Square then cascades up    
   >
     {props.value}
   </button>
